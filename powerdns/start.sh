1fe07478295b6341065219214a9682c9ab27fc70
Dimitris Zervas <d.zervas@encodegroup.com>
2017-01-26 18:07:01 +0200

    Debian-based image (not tested, docker registry is down)

--- powerdns/start.sh
+++ powerdns/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+mkdir -p /var/empty/data
 if [ ! -f /var/empty/data/powerdns.sqlite ]; then
 	echo "Creating PowerDNS database..."
 	touch /var/empty/data/powerdns.sqlite
 	sqlite3 /var/empty/data/powerdns.sqlite < /create_db.sql
 fi
+
+chown pdns:pdns /var/empty/data/ -R
 
 exec pdns_server --daemon=no --allow-recursion=172.17.0.0/24 \
 	--disable-axfr=yes --local-address=0.0.0.0 --launch=gsqlite3 \
