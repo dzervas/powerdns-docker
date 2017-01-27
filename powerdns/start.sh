6d112d5fed4939c9b68c732923e550c0eccd4206
Dimitris Zervas <d.zervas@encodegroup.com>
2017-01-27 14:12:50 +0200

    Fix powerdns with debian image

--- powerdns/start.sh
+++ powerdns/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-mkdir -p /var/empty/data
-if [ ! -f /var/empty/data/powerdns.sqlite ]; then
+mkdir -p /etc/powerdns/sqlite3/
+if [ ! -f /etc/powerdns/sqlite3/powerdns.sqlite ]; then
 	echo "Creating PowerDNS database..."
-	touch /var/empty/data/powerdns.sqlite
-	sqlite3 /var/empty/data/powerdns.sqlite < /create_db.sql
+	touch /etc/powerdns/sqlite3/powerdns.sqlite
+	sqlite3 /etc/powerdns/sqlite3/powerdns.sqlite < /create_db.sql
 fi
 
-chown pdns:pdns /var/empty/data/ -R
+chown pdns:pdns /etc/powerdns/sqlite3/ -R
 
 exec pdns_server --daemon=no --allow-recursion=172.17.0.0/24 \
 	--disable-axfr=yes --local-address=0.0.0.0 --launch=gsqlite3 \
-	--gsqlite3-database=/data/powerdns.sqlite --api=yes \
+	--gsqlite3-database=/etc/powerdns/sqlite3/powerdns.sqlite --api=yes \
 	--api-key=${API_KEY} --webserver=yes --webserver-address=0.0.0.0 \
 	--webserver-port=80 --webserver-password=${API_KEY} "$@"
 
